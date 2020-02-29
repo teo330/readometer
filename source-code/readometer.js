474d37e8e0c25be201f0f31d8d26cf569ff18324
teo330 <thewebcorsair@gmail.com>
2020-02-29 17:32:15 +0100

    Added min/mins loops and minor fixes

--- source-code/readometer.js
+++ source-code/readometer.js
@@ -3,34 +3,40 @@ function calcTime() {
 	var textbox = document.getElementById("textbox").value;
 	console.log("textbox: '" + textbox + "'");
 
-	var str1 = textbox.trim();
-	console.log("str1: '" + str1 + "'");
+	if (textbox.includes("\n") == true) {
+		console.log("replacing new lines with whitespace from textbox (then str1)...");
 
-	if (str1.includes("\n") == true) {
-		console.log("switching new lines with whitespaces...");
+		var str1 = textbox.replace(/\n+/g, " ");
 
-		var strLines = str1.replace(/\n+/g, " ");
-
-		console.log("temp Lines: '" + strLines + "'");
-	}
-	else {
-		console.log("no new line to be switched\nstrLines = str1");
-		strLines = str1;
+		console.log("str1: '" + str1 + "'");
+	} else {
+		console.log("no new line to be replaced\nstr1 = textbox");
+		str1 = textbox;
 	}
 
-	if (strLines.includes("  ") == true) {
-		console.log("removing redundant whitespaces...");
+	if (str1.includes(".") || str1.includes(",") || str1.includes("-") == true) {
+		console.log("replacing special characters from str1 (then str2)...");
 
-		var str2 = strLines.replace(/\s\s+/g, " ");
-		console.log("temp Rep: '" + str2 + "'");
+		var str2 = str1.replace(/\.+|\,+|\s\-+|\-+\s|\s\-+\s/g, " ");
+		console.log("str2: '" + str2 + "'");
+	} else {
+		console.log("no special characters to be replaced\nstr2 = str1");
+		str2 = str1
 	}
-	else {
-		console.log("no redundant whitespaces\nstr2 = strLines");
-		str2 = strLines;
+
+	if (str2.includes("  ") == true) {
+		console.log("removing redundant whitespaces from str2 (then str1)...");
+
+		var str1 = str2.replace(/\s\s+/g, " ");
+		console.log("str1 '" + str1 + "'");
+	} else {
+		console.log("no redundant whitespaces to be removed\nstr1 = str2");
+		str1 = str2;
 	}
 
-
-	console.log("strLines: '" + str2 + "'");
+	console.log("trimming the whitespaces from str1 (then str2)...");
+	var str2 = str1.trim();
+	console.log("str2: '" + str2 + "'");
 
 	var totWords = str2.split(" ")/*.length*/;
 	console.log("totWords: " + totWords);
@@ -49,11 +55,47 @@ function calcTime() {
 	console.log(avgRead);
 	avgRead.innerHTML = "~" + (parseInt(totWords / wpmAvg) + 1);
 
+	console.log("going to choose avg MIN/MINS...");
+	var areAvgMins = document.getElementById("areAvgMins");
+	if (parseInt(totWords / wpmAvg) == 0) {
+		console.log("joined avg loop MIN...");
+
+		areAvgMins.innerHTML = "min";
+	} else {
+		console.log("joined avg loop MINS...");
+
+		areAvgMins.innerHTML = "mins";
+	}
+
 	var minRead = document.getElementById("minRead");
 	console.log(minRead);
 	minRead.innerHTML = "~" + (parseInt(totWords / wpmMin) + 1);
 
+	console.log("going to choose min MIN/MINS...");
+	var areMinMins = document.getElementById("areMinMins");
+	if (parseInt(totWords / wpmMin) == 0) {
+		console.log("joined min loop MIN...");
+
+		areMinMins.innerHTML = "min";
+	} else {
+		console.log("joined min loop MINS...");
+
+		areMinMins.innerHTML = "mins";
+	}
+
 	var maxRead = document.getElementById("maxRead");
 	console.log(maxRead);
 	maxRead.innerHTML = "~" + (parseInt(totWords / wpmMax) + 1);
+
+	console.log("going to choose max MIN/MINS...");
+	var areMaxMins = document.getElementById("areMaxMins");
+	if (parseInt(totWords / wpmMax) == 0) {
+		console.log("joined max loop MIN...");
+
+		areMaxMins.innerHTML = "min";
+	} else {
+		console.log("joined max loop MINS...");
+
+		areMaxMins.innerHTML = "mins";
+	}
 }
